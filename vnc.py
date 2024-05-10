d70a55ecf025b48f27e3b26f56b827279315322c
nekootekii <152562452+nekootekii@users.noreply.github.com>
2024-05-10 09:12:07 +0700

    hú

--- vnc.py
+++ vnc.py
@@ -1,5 +1,6 @@
 import os
 
+print("Code by DuonngwTek [CatTek]")
 cmd = 'sudo apt update'
 os.system(cmd)
 cmd = 'sudo apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver'
@@ -14,4 +15,8 @@ cmd = 'sudo apt-get install vim-gtk3 -y'
 os.system(cmd)
 cmd = 'sudo apt install iputils-ping -y'
 os.system(cmd)
-print("vnc up")
+cmd = 'wget -O cr.deb "https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb"'
+os.system(cmd)
+cmd = 'sudo apt-get install ./cr.deb'
+os.system(cmd)
+print("Code by DuonngwTek [CatTek]")
