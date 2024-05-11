cc2c262ecae49c3a9802b5bc629d3911ac7221ca
nekootekii <152562452+nekootekii@users.noreply.github.com>
2024-05-11 18:06:27 +0700

    Done?

--- vnc.py
+++ vnc.py
@@ -8,8 +8,6 @@ cmd = 'sudo apt-get install geany -y'
 os.system(cmd)
 cmd = 'sudo apt-get install vim-gtk3 -y'
 os.system(cmd)
-cmd = 'sudo apt-get install vim-gtk3 -y'
-os.system(cmd)
 cmd = 'wget -O cr.deb "https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb"'
 os.system(cmd)
 cmd = 'sudo apt-get install ./cr.deb'
