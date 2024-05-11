cf36c6268c4d8316e926244ae12b728b0004db6a
nekootekii <152562452+nekootekii@users.noreply.github.com>
2024-05-11 17:50:46 +0700

    Update vnc.py

--- vnc.py
+++ vnc.py
@@ -1,22 +1,17 @@
 import os
 
-print("Code by DuonngwTek [CatTek]")
-cmd = 'sudo apt update'
+cmd = 'sudo apt-get update'
 os.system(cmd)
-cmd = 'sudo apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver'
-os.system(cmd)
-cmd = 'sudo apt install xfce4-terminal -y'
-os.system(cmd)
-cmd = 'sudo apt install firefox-esr -y'
+cmd = 'sudo apt-get install ubuntu-gnome-desktop -y'
 os.system(cmd)
 cmd = 'sudo apt-get install geany -y'
 os.system(cmd)
 cmd = 'sudo apt-get install vim-gtk3 -y'
 os.system(cmd)
-cmd = 'sudo apt install iputils-ping -y'
+cmd = 'sudo apt-get install vim-gtk3 -y'
 os.system(cmd)
 cmd = 'wget -O cr.deb "https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb"'
 os.system(cmd)
 cmd = 'sudo apt-get install ./cr.deb'
 os.system(cmd)
-print("Code by DuonngwTek [CatTek]")
+print("Complete!")
