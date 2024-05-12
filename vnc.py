c9a726a3175c6959e702f73acbb3f6b57e48b57e
nekootekii <152562452+nekootekii@users.noreply.github.com>
2024-05-12 16:15:22 +0700

    hết cuus :]]]

--- vnc.py
+++ vnc.py
@@ -8,9 +8,11 @@ cmd = 'sudo apt-get install geany -y'
 os.system(cmd)
 cmd = 'sudo apt-get install vim-gtk3 -y'
 os.system(cmd)
-cmd = 'sudo apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver'
+cmd = 'wget -O rm.deb "https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb"'
 os.system(cmd)
-cmd = 'wget -O cr.deb "https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb"'
+cmd = 'sudo apt-get install ./rm.deb'
+os.system(cmd)
+cmd = 'wget -O cr.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"'
 os.system(cmd)
 cmd = 'sudo apt-get install ./cr.deb'
 os.system(cmd)
